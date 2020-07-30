5668e32904a1dc108fe7c120fd8ed92e71a5c6e1
Naywia <maya@wettendorff.com>
2020-07-30 10:27:43 +0200

    End of project

--- calculate.php
+++ calculate.php
@@ -1,8 +1,14 @@
 <?php
+
 session_start();
 
+$one = filter_input(INPUT_GET, "one");
 $c = filter_input(INPUT_GET, "c");
+$val1 = filter_input(INPUT_GET, "val1");
+$val2 = filter_input(INPUT_GET, "val2");
 
+$_SESSION['selected1'] = $val1;
+$_SESSION['selected2'] = $val2;
 
 $rateX = $_SESSION['selected1'];
 $rateZ = $_SESSION['selected2'];
@@ -14,10 +20,15 @@ if (strstr($rateZ, ",")) {
     $rateZ = str_replace(",", ".", $rateZ); // replace ',' with '.'
 }
 
-$numberY = $c / 100 * $rateX;
-$number2 = $numberY / $rateZ * 100;
-$result = bcdiv($number2, 1, 2);
-
+if ($one == "true") {
+    $numberY = $c / 100 * $rateX;
+    $number2 = $numberY / $rateZ * 100;
+    $result = bcdiv($number2, 1, 2);
+} else if ($one == "false") {
+    $numberY = $c / 100 * $rateZ;
+    $number2 = $numberY / $rateX * 100;
+    $result = bcdiv($number2, 1, 2);
+}
 
 echo $result == "" ? "no suggestions" : $result;
 ?>
--- index.php
+++ index.php
@@ -15,20 +15,38 @@ session_start();
         <script type="text/javascript">
             function update(int) {
                 var val1 = document.getElementById('currency1').value;
-                if(int.lenght === 0){
+                var val2 = document.getElementById('currency2').value;
+                if (int.lenght === 0) {
                     document.getElementById("number2").value = "";
                     return;
-                } else{
+                } else {
                     var xmlhttp = new XMLHttpRequest();
-                    xmlhttp.onreadystatechange = function(){
-                        if(this.readyState === 4 && this.status === 200){
+                    xmlhttp.onreadystatechange = function () {
+                        if (this.readyState === 4 && this.status === 200) {
                             document.getElementById("number2").value = this.responseText;
                         }
                     };
-                    xmlhttp.open("GET", "calculate.php?c=" + int, true);
+                    xmlhttp.open("GET", "calculate.php?c=" + int + "&val1=" + val1 + "&val2=" + val2 + "&one=true", true);
                     xmlhttp.send();
                 }
+            }
 
+            function update2(int) {
+                var val1 = document.getElementById('currency1').value;
+                var val2 = document.getElementById('currency2').value;
+                if (int.lenght === 0) {
+                    document.getElementById("number1").value = "";
+                    return;
+                } else {
+                    var xmlhttp = new XMLHttpRequest();
+                    xmlhttp.onreadystatechange = function () {
+                        if (this.readyState === 4 && this.status === 200) {
+                            document.getElementById("number1").value = this.responseText;
+                        }
+                    };
+                    xmlhttp.open("GET", "calculate.php?c=" + int + "&val1=" + val1 + "&val2=" + val2 + "&one=false", true);
+                    xmlhttp.send();
+                }
             }
         </script>
     </head>
@@ -42,59 +60,62 @@ session_start();
             <?php
             $url = "https://www.nationalbanken.dk/_vti_bin/DN/DataService.svc/CurrencyRatesXML?lang=da";
             $xml = simplexml_load_file($url);
+
             if (empty($_SESSION['selected1'])) {
-                $_SESSION['selected1'] = "635,22";
+                $_SESSION['selected1'] = NULL;
             }
             if (empty($_SESSION['selected2'])) {
                 $_SESSION['selected2'] = 100;
             }
             ?>
 
-                <div class="numbers">
-                    <div class='firstNumber'>
-                        <div class="numb">
-                            <input class="values" name='number1' type='number' onkeyup="update(this.value)" step='0.01'>
-                        </div>
-                        <select name='currency1' id='currency1'  class="currency">
-                            <?php
-                            foreach ($xml->dailyrates->currency as $currency) {
-                                echo "<option";
-                                if ($_SESSION['selected1'] == $currency['rate']) {
-                                    echo " selected";
-                                }
-                                echo" value='" . $currency['rate'] . "'>" . $currency['desc'] . "</option>";
-                            }
-                            if ($_SESSION['selected1'] != 100) {
-                                echo "<option value='100'>Danske Kroner</option>";
-                            } else {
-                                echo "<option value='100' selected>Danske Kroner</option>";
-                            }
-                            ?>
-                        </select>
+            <div class="numbers">
+                <div class='firstNumber'>
+                    <div class="numb">
+                        <input class="values" name='number1' id='number1' type='number' onkeyup="update(this.value)" step='0.01'>
                     </div>
-
-                    <div class='secondNumber'>
-                        <div class="numb">
-                            <input class="values" name='number2' id='number2' type='number' step='0.01'>
-                        </div>
-                        <select name='currency2' id='currency2' class="currency">
-                            <?php
-                            foreach ($xml->dailyrates->currency as $currency) {
-                                echo "<option";
-                                if ($_SESSION['selected2'] == $currency['rate']) {
-                                    echo " selected";
-                                }
-                                echo" value='" . $currency['rate'] . "'>" . $currency['desc'] . "</option>";
+                    <select name='currency1' id='currency1'  class="currency">
+                        <?php
+                        foreach ($xml->dailyrates->currency as $currency) {
+                            echo "<option";
+                            if ($_SESSION['selected1'] == NULL && $currency['code'] == "USD") {
+                                echo " selected";
+                            } else if ($_SESSION['selected1'] == $currency['rate']) {
+                                echo " selected";
                             }
-                            if ($_SESSION['selected2'] != 100) {
-                                echo "<option value='100'>Danske Kroner</option>";
-                            } else {
-                                echo "<option value='100' selected>Danske Kroner</option>";
-                            }
-                            ?>
-                        </select>
-                    </div>
+                            echo" value='" . $currency['rate'] . "'>" . $currency['desc'] . "</option>";
+                        }
+                        if ($_SESSION['selected1'] != 100) {
+                            echo "<option value='100'>Danske Kroner</option>";
+                        } else {
+                            echo "<option value='100' selected>Danske Kroner</option>";
+                        }
+                        ?>
+                    </select>
                 </div>
+
+                <div class='secondNumber'>
+                    <div class="numb">
+                        <input class="values" name='number2' id='number2' type='number' onkeyup="update2(this.value)" step='0.01'>
+                    </div>
+                    <select name='currency2' id='currency2' class="currency">
+                        <?php
+                        foreach ($xml->dailyrates->currency as $currency) {
+                            echo "<option";
+                            if ($_SESSION['selected2'] == $currency['rate']) {
+                                echo " selected";
+                            }
+                            echo" value='" . $currency['rate'] . "'>" . $currency['desc'] . "</option>";
+                        }
+                        if ($_SESSION['selected2'] != 100) {
+                            echo "<option value='100'>Danske Kroner</option>";
+                        } else {
+                            echo "<option value='100' selected>Danske Kroner</option>";
+                        }
+                        ?>
+                    </select>
+                </div>
+            </div>
         </div>
     </body>
 </html>
